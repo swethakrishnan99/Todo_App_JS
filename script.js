4acda10efdc5f0a9c359e56c5f9bb8f1a7abc0a1
swethakrishnan99 <82903764+swethakrishnan99@users.noreply.github.com>
2021-10-11 21:46:27 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,157 +1,199 @@
-let objList =[];
-function newList() {
-  document.getElementById("newListPage").style.display = "inherit";
-  document.getElementById("homePage").style.filter = "blur(8px)";
-}
-function backHome(){
-    document.getElementById("homePage").style.display = "inherit";
-     document.getElementById("newListPage").style.display = "none";
-     document.getElementById("newItemPage").style.display = "none";
-     document.getElementById("listPage").style.display = "none";
-     document.getElementById("homePage").style.filter = "blur(0px)";
-     document.getElementById("listPage").style.filter = "blur(0px)";
-}
-
-function createList(){
-    var obj ={};
-    var id = new Date ().valueOf();
-    var clicked =false;
-    var items =[];
-    let listName = document.getElementById("inputList").value;
-    if(listName.trim()!==""){
-        let listBox = document.createElement("div");
-        listBox.id = id;
-        listBox.className = "listBoxSmall";
-        listBox.innerHTML =`<p class="listTitle" onclick="displayBig(this.parentNode.id)">${listName} 
-        </p><hr><span class="listButtons small"><i class="fas fa-trash-alt red" id="trash" 
-        onclick="deleteList(this.parentNode.parentNode.id, this.parentNode.parentNode.className)">
-        </i>&ensp;<i class="fas fa-plus-circle blue" id="add" onclick="newItem(this.parentNode.parentNode.id, 
-        this.parentNode.parentNode.className)"></span>`
-        document.getElementById("array").appendChild(listBox);
-        backHome();
-        obj.id = id;
-        obj.name = listName;
-        obj.click = clicked;
-        obj.items =items;
-        obj.innerHTML = listBox.innerHTML;
-        objList.push(obj);
-    }
-    else{
-        alert("Please fill the input field!");
-    }
-}
-function displayBig(parentId){
-  let index = -1;
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("listPage").style.display = "flex";
-    for(let i =0; i<objList.length;i++){
-        if(objList[i].id==parentId){
-          index = i;
-            document.getElementById("listPageBox").innerHTML =objList[i].innerHTML;
-              document.getElementById("listPageBox").className = parentId;
-            document.getElementById("listPageHead").innerHTML =`<hr><span>${objList[i].name}</span>
-            <i class="fas fa-arrow-circle-left" onclick="backHome()">&ensp;<span class="iconname">BACK</span></i>`;
-        }
-    }
-    for(let i=0; i<objList[index].items.length;i++){
-    if(objList[index].items[i].completed ==true){
-      console.log("true");
-      document.getElementById(objList[index].items[i].id).style.textDecoration = "line-through";
-      document.getElementById(objList[index].items[i].id).style.color ="red";
-      document.getElementById(objList[index].items[i].id).innerHTML=objList[index].items[i].item;
-    }
+let itemsList =[];
+let currentList;
+function noLists(){
+  if(itemsList.length !== 0){
+    document.getElementById("noList").style.display ="none";
+  }
+  else{
+    document.getElementById("noList").style.display ="block";
   }
 }
-function deleteList(id, classname){
-    if(id ==="listPageBox"){
-        id =classname;
-        backHome()
-    }
-    var listBox = document.getElementById(id);
-    listBox.remove();
-      for(let i =0; i<objList.length;i++){
-        if(objList[i].id==id){
-            objList.splice(i,1);
-        }
-      }
+    
+function newlistPopup() {
+  document.getElementById("homePage").classList.add("blur");
+  document.getElementById("newListPopup").classList.add("popUp");
+  document.getElementById("newListPopup").classList.remove("none");
 }
 
-function newItem(id, classname){
- 
-  if(id ==="listPageBox"){
-        id =classname;
+function newItemPopup(flag,element){
+  currentList =element;
+  console.log(element);
+  console.log(flag);
+  document.getElementById("newItemPopup").classList.add("popUp");
+  document.getElementById("newItemPopup").classList.remove("none");
+  if (flag ===0){
+    document.getElementById("homePage").classList.add("blur");
   }
-  for(let i =0; i<objList.length;i++){
-        if(objList[i].id==id){
-            objList[i].click=true;
-        }
-    }
-  document.getElementById("newItemPage").style.display = "inherit";
-  document.getElementById("homePage").style.filter = "blur(8px)";
-  document.getElementById("listPage").style.filter = "blur(8px)";
+  else{
+    document.getElementById("selectedTodo").classList.add("blur");
+  }
 }
-function createItem(){
-  let index =-1;
-  let parentId = -1;
-  let id ="0"
-  let value = document.getElementById("inputItem").value;
-  let list = document.createElement("ul");
-  let itemvalues ="";
-  for(let i =0; i<objList.length;i++){
-      if(objList[i].click===true){
-          index =i;
-          objList[i].items.push({"id":`${index}_${objList[i].items.length}`,"item":value, completed :false});
-          objList[i].click=false;
-          parentId = objList[i].id;
-          id +=parentId;
-          break;
-      }
-    }
-    list.id =id;
-    document.getElementById(parentId).appendChild(list);
-    for(let i =0; i<objList[index].items.length;i++){
-        itemvalues += `<li id="${objList[index].items[i].id}">${objList[index].items[i].item}&ensp;
-        <button onclick="markDone(this)">Mark Done</button></li>`
-    }
-    document.getElementById(id).innerHTML =itemvalues;
-    objList[index].innerHTML =document.getElementById(parentId).innerHTML;
-    if(document.getElementById("homePage").style.display === "none"){
-      displayBig(parentId);
-    }
-    for(let i=0; i<objList[index].items.length;i++){
-    if(objList[index].items[i].completed ==true){
-      document.getElementById(objList[index].items[i].id).style.textDecoration = "line-through";
-      document.getElementById(objList[index].items[i].id).style.color ="red";
-      document.getElementById(objList[index].items[i].id).innerHTML=objList[index].items[i].item;
-    }
-  }
-    document.getElementById("newItemPage").style.display = "none";
-    document.getElementById("homePage").style.filter = "blur(0px)";
-    document.getElementById("listPage").style.filter = "blur(0px)";
 
-}
-function markDone(value){
-  let id =value.parentNode.id;
-  console.log(id)
-  let parentId = value.parentNode.parentNode.id;
-  let index1 =-1;
+function backHome(flag){
+  if(flag===0){
+    document.getElementById("newListPopup").classList.add("none");
+    document.getElementById("homePage").classList.remove("blur");
+  }
+  else if(flag===1){
+    document.getElementById("homePage").style.display ="block";
+    document.getElementById("selectedTodo").classList.add("none");
+    document.getElementById("selectedTodo").classList.remove("container");
+    createCards();    
+  }
+  else{
+    document.getElementById("newItemPopup").classList.add("none");
+    document.getElementById("selectedTodo").classList.remove("blur");
+      document.getElementById("homePage").classList.remove("blur");
+  }
   
-  for(let i=0; i<objList.length; i++){
-    if (objList[i].id == parentId){
-       index1 =i;
-    }
-  }
-  for(let i=0; i<objList[index1].items.length;i++){
-    if(objList[index1].items[i].id==id){
-      objList[index1].items[i].completed =true;
-    }
-  }
-  for(let i=0; i<objList[index1].items.length;i++){
-    if(objList[index1].items[i].completed ==true){
-      document.getElementById(objList[index1].items[i].id).style.textDecoration = "line-through";
-      document.getElementById(objList[index1].items[i].id).style.color ="red";
-      document.getElementById(objList[index1].items[i].id).innerHTML=objList[index1].items[i].item;
-    }
-  }
-
+  noLists();
 }
+
+function addToList(){
+  let heading = document.getElementById("input1").value;
+  if(heading.trim()===""){
+    alert("please fill the inputfield");
+  }
+  else{
+    const obj = {
+      id : Date.now(),
+      heading,
+      taskList :[]
+    }
+    itemsList.push(obj);
+    backHome(0)
+    const node = document.createElement("div");
+    node.setAttribute("class", `card`);
+    node.setAttribute("data-key", itemsList[itemsList.length-1].id);
+    node.innerHTML = `<p class="card-heading" onclick = "redirect(this)">${itemsList[itemsList.length-1].heading} </p>
+    <hr>
+    <div class="footer-1">
+    <p class='btn-completed' onclick ="removeList(this)"><i class="fas fa-trash-alt" ></i></p>
+    <p class='btn-add' onclick ="newItemPopup(0,this)"><i class="fas fa-plus-circle " ></i></p>
+    </div>
+    <ul style="list-style-type:none;"></ul>`;
+    document.getElementById("lists").appendChild(node);
+  }
+}
+function createCards(){
+  document.getElementById("lists").innerHTML = "";
+  for(let i =0;i<itemsList.length;i++){
+    const node = document.createElement("div");
+    node.setAttribute("class", `card`);
+    node.setAttribute("data-key", itemsList[i].id);
+    node.innerHTML = `<p class="card-heading"  onclick = "redirect(this)">${itemsList[i].heading}</p>
+    <hr>
+    <div class="footer-1">
+    <p class='btn-completed' onclick ="removeList(this)"><i class="fas fa-trash-alt"></i></p>
+    <p class='btn-add' onclick ="newItemPopup(0,this)"><i class="fas fa-plus-circle " ></i></p>
+    </div>
+    <ul style="list-style-type:none;"></ul>`;
+    document.getElementById("lists").appendChild(node);
+    currentList=itemsList[i];
+    for(let j=0;j<currentList.taskList.length;j++){
+      const node1 = document.createElement("li");
+      node1.setAttribute("class", `item`);
+      node1.setAttribute("data-key",currentList.taskList[j].id);
+      if(currentList.taskList[j].completed == true){
+         node1.innerHTML =`${currentList.taskList[j].task}`;
+         node1.style.textDecoration="line-through";
+         node1.style.color ="red";
+      }
+      else{
+        node1.innerHTML =`${currentList.taskList[j].task}<button class="marked" onclick="markedDone(this)">Mark Done</button>`;
+      }
+     node.childNodes[6].appendChild(node1);
+    }
+  }
+}
+    
+function removeList(element){
+  parentElement = element.parentNode.parentNode;
+  let temp = parentElement.getAttribute("class");
+   for(let i=0;i<itemsList.length;i++) {
+      if (itemsList[i].id == parentElement.getAttribute("data-key")){
+        itemsList.splice(i,1);
+      }
+    }
+    createCards()
+  if(temp != "card"){
+      backHome(1);
+  }
+   noLists()
+}
+
+function redirect(element){
+  const id = element.parentNode.getAttribute("data-key");
+  for(let i=0;i<itemsList.length;i++){
+    if(itemsList[i].id == id){
+      currentList=itemsList[i];
+    }
+  }
+  document.getElementById("selectedList").innerHTML="";
+  for(let j=0;j<currentList.taskList.length;j++){
+      const node1 = document.createElement("li");
+      node1.setAttribute("class", `item`);
+      node1.setAttribute("data-key",currentList.taskList[j].id);
+      if(currentList.taskList[j].completed == true){
+        node1.innerHTML =`${currentList.taskList[j].task}`;
+        node1.style.textDecoration="line-through";
+        node1.style.color ="red";
+      }
+      else{
+        node1.innerHTML =`${currentList.taskList[j].task}<button class="marked" onclick="markedDone(this)">Mark Done</button>`;
+      };
+      document.getElementById("selectedList").appendChild(node1);
+    }
+
+  document.getElementById("selectedHeading").innerHTML = currentList.heading;
+  document.getElementById("selectedHeading1").innerHTML = currentList.heading;
+  document.getElementById("selectedHeading1").parentNode.setAttribute("data-key",id);
+  document.getElementById("homePage").style.display ="none";
+  document.getElementById("selectedTodo").classList.add("container");
+  document.getElementById("selectedTodo").classList.remove("none");
+  
+}
+
+function addToItems(){
+  let task = document.getElementById("input2").value;
+  if(task.trim()===""){
+    alert("please fill the inputfield");
+  }
+  else{
+    let taskobj ={task,completed: false,id:Date.now()}
+    let index1;
+    let parentId = currentList.parentNode.parentNode.getAttribute("data-key");
+    for(let i =0;i<itemsList.length;i++){
+      if(itemsList[i].id ==parentId){
+        itemsList[i].taskList.push(taskobj);
+        index1 = itemsList[i]
+      }
+    }
+    const node = document.createElement("li");
+    node.setAttribute("class", `item`);
+    node.setAttribute("data-key", index1.taskList[index1.taskList.length-1].id);
+    node.innerHTML =`${index1.taskList[index1.taskList.length-1].task}<button class="marked" onclick = "markedDone(this)">Mark Done</button>`;
+    currentList.parentNode.parentNode.childNodes[6].appendChild(node);
+    backHome(3)
+  }
+}
+
+function markedDone(element){
+  id = element.parentNode;
+  parentId =element.parentNode.parentNode.parentNode;
+  console.log(parentId);
+  for(let i =0;i<itemsList.length;i++){
+    if(itemsList[i].id ==parentId.getAttribute("data-key")){
+      for(j=0;j<itemsList[i].taskList.length;j++){
+        if(itemsList[i].taskList[j].id == id.getAttribute("data-key")){
+          itemsList[i].taskList[j].completed =true;
+          element.parentNode.style.textDecoration ="line-through";
+          element.parentNode.style.color="red";
+          element.remove();
+        }
+      }
+
+    }
+  }
+}
+ 
